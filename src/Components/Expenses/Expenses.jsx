45e1dcabfde1f521663e4e5e20e81ab1384b031f
sujay kumar <sujaynandhakumar96@gmail.com>
2023-02-11 14:39:25 +0530

    state implementation example

--- src/Components/Expenses/Expenses.jsx
+++ src/Components/Expenses/Expenses.jsx
@@ -1,3 +1,4 @@
+import React,{ useState } from "react";
 import ExpenseItem from "./ExpenseItem/ExpenseItem";
 
 
@@ -8,14 +9,23 @@ const Expenses = ()=>{
         amount:100
     }];
 
+    let [addShowState,setShowState] = useState(false);
+
+   const changeState = ()=>{
+        setShowState(!addShowState);
+    }
 
     return (
         <div className="container">
             <h1>Expenses loaded</h1>
+            <button onClick={changeState()}>Show State</button>
+            <div className="row mb-3 mt-2">
+                {addShowState?'Show state working':null}
+            </div>
             <div className="row">
             <ExpenseItem expnese={expnese[0]}></ExpenseItem>
-            <ExpenseItem expnese={expnese[0]}></ExpenseItem>
-            <ExpenseItem expnese={expnese[0]}></ExpenseItem>
+            {/* <ExpenseItem expnese={expnese[0]}></ExpenseItem>
+            <ExpenseItem expnese={expnese[0]}></ExpenseItem> */}
             </div>
         </div>
     )
