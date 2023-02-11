08348cf57b267060dc8683c411fb6ac9266c97b1
sujay kumar <sujaynandhakumar96@gmail.com>
2023-02-11 16:12:07 +0530

    key is for to avoid error unique value

--- src/Components/Expenses/Expenses.jsx
+++ src/Components/Expenses/Expenses.jsx
@@ -37,7 +37,7 @@ const Expenses = ()=>{
 
             </div>
             <div className="row">
-                {expnese.map(e=> <ExpenseItem expnese={e}></ExpenseItem>)}
+                {expnese.map(e=> <ExpenseItem expnese={e} key={e.id}></ExpenseItem>)}
             </div>
         </div>
     )
