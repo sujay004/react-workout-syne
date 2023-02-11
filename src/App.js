43a89bf39c391b5c491b73fde233b1d9a7391beb
sujay kumar <sujaynandhakumar96@gmail.com>
2023-02-11 11:58:53 +0530

    Object binding in component exaple

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import React from 'react';
 
 function App() {
+  let user ={firstName:'sujay'};
+
   return (
-    // <div className="App">
-    //   <h1>Sujay</h1>
-    // </div>
-    React.createElement('div',{},
-    React.createElement('h1',{},'Functional componenet'))
+    <div className="App">
+      <p>{user.firstName.toUpperCase()}</p>
+    </div>
+ //This is jsx syntax
+
   );
 }
 
 export default App;
+   // React.createElement('div',{},
+    // React.createElement('h1',{},'Functional componenet'))
+     //This is js syntax it will be complicated when we have so much elements
